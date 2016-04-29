718c4aaf4519a0116f4e03254051488a9d93d444
AChi <unique.ntsn+git@gmail.com>
2016-04-29 15:37:16 +0800

    add files

--- php/address_vs_value.php
+++ php/address_vs_value.php
@@ -15,18 +15,20 @@ and open the template in the editor.
             //參數傳遞值的函數
             function func_pass_value($para){
                 $para*=2;
-                echo 'inside: ' . $para . '<br>';
+                echo '參數傳遞值inside: ' . $para;
             }
             func_pass_value($x1);
-            echo 'outside: ' . $x1;
-            
-            //參數傳遞記憶體位置的函數
-            function func_pass_address($para){
+            echo '<br>';
+            echo '參數傳遞值outside: ' . $x1;
+            //參數傳遞記憶體位置的函數，方法是在定義參數時在'$'之前加上一個'&'符號
+            function func_pass_address(&$para){
                 $para*=2;
-                echo 'inside: ' . $para . '<br>';
+                echo '參數傳遞記憶體位置inside: ' . $para;
             }
-            func_pass_address($x1);
-            echo 'outside: ' . $x1;
+            echo '<br>';
+            func_pass_address($x1);//此時傳入的是$x1變數的記憶體位置，這會讓函數裡的區域變數指定到同一個位置，對該變數指定值會讓$x1的值也改變
+            echo '<br>';
+            echo '參數傳遞記憶體位置outside: ' . $x1;
         ?>
     </body>
 </html>
